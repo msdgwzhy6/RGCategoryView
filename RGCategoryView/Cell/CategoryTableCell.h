45cdca3656e3dd10670fa969cbbc97b466a6aad5
arvin <rorogestudio@gmail.com>
2015-10-28 16:29:58 +0800

    add tableview

---
+++ RGCategoryView/Cell/CategoryTableCell.h
@@ -0,0 +1,13 @@
+//
+//  CategoryTableCell.h
+//  RGCategoryView
+//
+//  Created by Arvin on 15/10/28.
+//  Copyright © 2015年 com.roroge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CategoryTableCell : UITableViewCell
+
+@end
---
+++ RGCategoryView/Cell/CategoryTableCell.m
@@ -0,0 +1,23 @@
+//
+//  CategoryTableCell.m
+//  RGCategoryView
+//
+//  Created by Arvin on 15/10/28.
+//  Copyright © 2015年 com.roroge. All rights reserved.
+//
+
+#import "CategoryTableCell.h"
+
+@implementation CategoryTableCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
